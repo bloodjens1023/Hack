4c8bc99411c4663b540bbbae99f432badb820295
bloodjens1023 <bloodjens32@gmail.com>
2024-02-10 18:12:33 +0100

    test

--- database.js
+++ database.js
@@ -8,21 +8,13 @@ const pool = mysql.createPool({
 }).promise()
 
 // Utilisez la fonction asynchrone pour pouvoir utiliser await
-async function donne() {
-    try {
-        const [rows, fields] = await pool.query("SELECT * FROM notes")
-        return rows
-    } catch (error) {
-        console.error("Erreur lors de l'exécution de la requête :", error)
-        return null
-    } finally {
-        // Assurez-vous de libérer la piscine après utilisation
-        pool.end()
-    }
+try {
+    const [rows, fields] = await pool.query("SELECT * FROM notes")
+    console.log(rows)
+} catch (error) {
+    console.error("Erreur lors de l'exécution de la requête :", error)
+    return null
+} finally {
+    // Assurez-vous de libérer la piscine après utilisation
+    pool.end()
 }
-
-module.exports = donne
-
-
-console.log("salut kaiza kaiza")
-
