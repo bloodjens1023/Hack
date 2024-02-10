76dff7494ec88d7e2db22c72b01c2adcbcd86aef
bloodjens1023 <bloodjens32@gmail.com>
2024-02-10 19:07:45 +0100

    sql

--- back.sql
+++ back.sql
@@ -14,16 +14,46 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Listage des données de la table back.comment : ~0 rows (environ)
-DELETE FROM `comment`;
+-- Listage de la structure de la table back. comment
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id_com` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_pub` int(11) DEFAULT 0,
+  `description_com` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_com`),
+  KEY `FK_pub` (`fk_pub`),
+  CONSTRAINT `FK_pub` FOREIGN KEY (`fk_pub`) REFERENCES `publication` (`id_pub`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Listage des données de la table back.publication : ~0 rows (environ)
-DELETE FROM `publication`;
+-- Les données exportées n'étaient pas sélectionnées.
 
--- Listage des données de la table back.user : ~1 rows (environ)
-DELETE FROM `user`;
-INSERT INTO `user` (`id_user`, `nom`, `prenom`, `localisation`, `cin`, `lieu_concerne`, `surete`) VALUES
-	(1, 'rakotohasimbola', 'jenny', '10,-10', '123456789', 'tana,mada,exemple', 0);
+-- Listage de la structure de la table back. publication
+CREATE TABLE IF NOT EXISTS `publication` (
+  `id_pub` int(11) NOT NULL AUTO_INCREMENT,
+  `titre_pub` varchar(100) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `gravite` enum('Jaune','Orange','Rouge') DEFAULT NULL,
+  `adore` int(11) DEFAULT NULL,
+  `fk_client` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_pub`),
+  KEY `FK_client` (`fk_client`),
+  CONSTRAINT `FK_client` FOREIGN KEY (`fk_client`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de la table back. user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(120) DEFAULT NULL,
+  `prenom` varchar(120) DEFAULT NULL,
+  `localisation` varchar(120) DEFAULT NULL,
+  `cin` varchar(120) DEFAULT NULL,
+  `lieu_concerne` varchar(120) DEFAULT NULL,
+  `surete` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
