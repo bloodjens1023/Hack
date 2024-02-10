e4183660397b37ca04f9fd68d9510e53b42cea35
bloodjens1023 <bloodjens32@gmail.com>
2024-02-10 20:19:29 +0100

    remodif

--- Entite/client.js
+++ Entite/client.js
@@ -4,6 +4,19 @@ class Client {
     constructor() {
 
     }
+
+    async insert_user(user) {
+        try {
+            // Utilisez une requête préparée pour insérer l'utilisateur dans la base de données
+            const sql = "INSERT INTO user (nom, prenom, localisation, cin, lieu_concerne, surete) VALUES (?, ?, ?, ?, ?, ?)";
+            const values = [user.nom, user.prenom, user.localisation, user.cin, user.lieu_concerne, user.surete];
+            const result = await pool.query(sql, values);
+            return result;
+        } catch (error) {
+            console.error("Erreur lors de l'insertion de l'utilisateur :", error);
+            return null;
+        }
+    }
     async selection_user() {
         try {
             const [rows, fields] = await pool.query("SELECT * FROM user");
--- server.js
+++ server.js
@@ -28,6 +28,24 @@ app.get('/api/auth', async (req, res) => {
     }
 });
 
+app.get('/api/inscr', async (req, res) => {
+    try {
+        const us = {
+            'nom': "jenny",
+            'prenom': "jenny",
+            'cin': "12344",
+            'localisation': "jenny",
+            'lieu_concerne': "jenny",
+            'surete': 0,
+        }
+        const data = await cl.insert_user(us); // Appeler la fonction donne dans votre route
+        res.json(data);
+    } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+        res.status(500).json({ error: "Une erreur s'est produite lors de la récupération des données." });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Le serveur est sur le port ${PORT}`);
 });
