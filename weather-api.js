2f91b376e5500dc0c1360476874606528fd1805f
Boy-With-Uke <satorug10@gmail.com>
2024-02-11 05:43:39 +0300

    nom->coordonee->meteo

--- weather-api.js
+++ weather-api.js
@@ -1,8 +1,33 @@
 
 const key = "aabad226ab52a9a2f0c635d062936c13";
-const geocodeCity = require('./test-api');
 const request = require("request");
+const opencage = require("opencage-api-client");
 
+function geocodeCity(cityName) {
+    return opencage
+      .geocode({ q: cityName, language: "fr" })
+      .then((data) => {
+        if (data.status.code === 200 && data.results.length > 0) {
+          const place = data.results[0];
+          const formatted = place.formatted;
+          const geometry = place.geometry;
+          const timezone = place.annotations.timezone.name;
+          return { formatted, geometry, timezone };
+        } else {
+          const error = data.status.message;
+          const total_results = data.total_results;
+          return { error, total_results };
+        }
+      })
+      .catch((error) => {
+        console.log("Error", error.message);
+        if (error.status.code === 402) {
+          console.log("hit free trial daily limit");
+          console.log("become a customer: https://opencagedata.com/pricing");
+        }
+        return { error: error.message };
+      });
+  }
 
 function getTempsActuel(lat, lon, key) {
   // Temps actuel
