98b0ddbd90eee09c7c345ba3a44bde26b2470012
Boy-With-Uke <satorug10@gmail.com>
2024-02-11 16:14:37 +0300

    api-weather

---
+++ client/src/api/weather.js
@@ -0,0 +1,18 @@
+const key = "aabad226ab52a9a2f0c635d062936c13";
+
+const request = require("request");
+
+function getTempsActuel(lat, lon, key) {
+  // Temps actuel plus prediction de tous les trois heures sur une marge de 5 heures
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const data = JSON.parse(body);
+      // Traitez les données de réponse ici
+      console.log(JSON.stringify(data, null, 2)); // Affiche les données formatées
+    } else {
+      console.log("Erreur:", error);
+    }
+  });
+}
